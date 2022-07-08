656db29b6bd6cc33207fa07e43710e27fe32627d
sj2sj <waterttug@naver.com>
2022-07-08 17:29:05 +0900

    first

---
+++ src/test/java/kr/co/searchstudy/SearchStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.co.searchstudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SearchStudyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
