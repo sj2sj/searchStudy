2bcd64d3cb25476f852c8068a0473aa304cc6877
sj2sj <waterttug@naver.com>
2022-07-12 19:01:23 +0900

    gitignore

--- src/main/java/kr/co/searchstudy/SearchStudyApplication.java
+++ src/main/java/kr/co/searchstudy/SearchStudyApplication.java
@@ -2,8 +2,10 @@ package kr.co.searchstudy;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
+@PropertySource(value = { "classpath:jdbc.properties" })
 public class SearchStudyApplication {
 
 	public static void main(String[] args) {
---
+++ src/main/java/kr/co/searchstudy/TestController.java
@@ -0,0 +1,17 @@
+package kr.co.searchstudy;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Controller
+public class TestController {
+
+	@RequestMapping("/main")
+	public static String goMain() {
+		// TODO Auto-generated method stub
+		return "contents/main";
+	}
+	
+
+}
