db107d960b62dcdfeeb5bebf4a42b5ad238b0ca2
sj2sj <waterttug@naver.com>
2022-07-23 14:55:45 +0900

    header css

--- src/main/java/kr/co/searchstudy/member/MemberController.java
+++ src/main/java/kr/co/searchstudy/member/MemberController.java
@@ -1,8 +1,18 @@
 package kr.co.searchstudy.member;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class MemberController {
 
+	
+	@ResponseBody
+	@GetMapping("/oauth/kakao")
+	public void kakaoLogin(@RequestParam String code) throws Exception {
+		System.out.println(code);
+	}
+	
 }
